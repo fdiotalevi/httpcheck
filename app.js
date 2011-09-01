f30cec6383ccc11c6a9c8d7176dad18d7127bbe3
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2011-09-01 11:55:30 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var _ = require('./underscore');
+
+var FullResponse = function(req) {
+  var url = req.url;
+  var httpVersion = req.httpVersion;
+  var method = req.method;
+  var headers = req.headers;
+  var data = "";
+  
+  var format = function(key, value) {
+    return key+": "+value+"\n";
+  }
+  
+  this.print = function() {
+    var output = format("url", url);
+    output += format("method", method);
+    output += format("httpVersion", httpVersion);
+    output += format("headers", _.reduce(_.keys(headers), function(value, it) {
+                        return value + format("   "+it, headers[it]);
+                      }, "\n"));
+    output += format("data", data);
+    return output;
+  }
+  
+  this.addData = function(it) {
+    data += it.toString("utf8");
+  }
+}
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var response = new FullResponse(req);  
+  req.on("data", function(it) {
+    response.addData(it);
+  });  
+  req.on("end", function() {
+    res.end(response.print());  
+  });  
+}).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');
+
+
+
