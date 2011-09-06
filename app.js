44f5e220f172293ecc93afab36f73d8218a88e5a
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2011-09-06 12:18:26 +0100

    refactored and compacted the code

--- app.js
+++ app.js
@@ -3,40 +3,35 @@ var _ = require('./underscore');
 var port = process.env.PORT || 3000;
 
 var FullResponse = function(req) {
-  var url = req.url;
-  var httpVersion = req.httpVersion;
-  var method = req.method;
-  var headers = req.headers;
-  var data = "";
+
+  var blanks = "                                   ";
+  var fields = {"url": req.url, "method": req.method, 
+              "httpVersion": req.httpVersion,
+              "headers": req.headers, "data": ""};  
+    
+  var format = function(key, value, indent) {
+    if (_.isString(value))
+      return blanks.substring(0, ++indent * 2) + key+": "+value+"\n"; 
+    
+    return blanks.substring(0, ++indent * 2) + key+": \n" + _.reduce(_.keys(value), function(sum, it) {
+                          return sum + format(it, value[it], indent);
+                        }, "");
+  }        
   
-  var format = function(key, value) {
-    return key+": "+value+"\n";
+  this.printOut = function() {        
+    return format("response", fields, -1);
   }
   
-  this.print = function() {
-    var output = format("url", url);
-    output += format("method", method);
-    output += format("httpVersion", httpVersion);
-    output += format("headers", _.reduce(_.keys(headers), function(value, it) {
-                        return value + format("   "+it, headers[it]);
-                      }, "\n"));
-    output += format("data", data);
-    return output;
-  }
-  
-  this.addData = function(it) {
-    data += it.toString("utf8");
-  }
 }
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   var response = new FullResponse(req);  
   req.on("data", function(it) {
-    response.addData(it);
+    fields["data"] += it.toString("utf8");
   });  
   req.on("end", function() {
-    res.end(response.print());  
+    res.end(response.printOut());  
   });  
 }).listen(port);
 console.log('Server running at port '+port);
