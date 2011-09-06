2c1f82d5f7b88fe2228b9d1a95561e5c07a5cbb0
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2011-09-06 17:49:12 +0100

    removed duplication in format method

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ var FullResponse = function(req) {
   var blanks = "                                   ";
   var fields = {"url": req.url, "method": req.method, "httpVersion": req.httpVersion, "headers": req.headers, "data": ""};  
     
-  var format = function(key, value, indent) {
+  var format = function(key, value, indent) {    
     if (_.isString(value))
-      return blanks.substring(0, indent++ * 2) + key+": "+value+"\n"; 
-    
-    return blanks.substring(0, indent++ * 2) + key+": \n" + _.reduce(_.keys(value), function(sum, it) {
-                          return sum + format(it, value[it], indent);
-                        }, "");
+      var valuePart = value+"\n";
+    else
+    {      
+      var valuePart = "\n" + _.reduce(_.keys(value), function(sum, it) {
+                                 return sum + format(it, value[it], indent+1);
+                               }, "");
+    }
+          
+    return blanks.substring(0, indent * 2) + key+": " + valuePart;
   }        
   
   this.printOut = function() {        
